036bef3c7e61045f65ee0c6e4262bd090205f30c
BigRiv101 <toledord@grace.edu>
2026-01-27 20:26:02 -0500

    Parser / Prompt

--- main.py
+++ main.py
@@ -1,8 +1,38 @@
 from dotenv import load_dotenv
 from pydantic import BaseModel
-from langchain_anthropic import ChatAnthropic
-from langchain_openai import ChatOpenAI
+import lmstudio as lms
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
 
-load_dotenv()  # Load environment variables from .env file
+load_dotenv()
 
-llm = ChatAnthropic(model="claude-4-5-sonnet-20250929")
+class ResearchResponse(BaseModel):
+    topics: str
+    summary: str
+    sources: list[str]
+    tools_used: list[str]
+
+
+
+
+api_host = lms.Client.find_default_local_api_host() or "http://127.0.0.1:4000"
+lms.configure_default_client(api_host)
+model = lms.llm()
+
+parser = PydanticOutputParser(pydantic_object=ResearchResponse)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            You are a research assistant that will help generate a research paper.
+            Answer the user query and use necessary tools. 
+            Wrap the output in this format and provide no other text\n{format_instructions}
+            """,
+        ),
+        ("placeholder", "{chat_history}"),
+        ("human", "{query}"),
+        ("placeholder", "{agent_scratchpad}"),
+    ]
+).partial(format_instructions=parser.get_format_instructions())
